b20c23a81f8783e168c9d0d2d2bad7499fbb19af
Ramprits <rampritsahani@outlook.in>
2019-06-28 18:58:43 +0530

    3 files modified

--- src/app/components/auth/auth.module.ts
+++ src/app/components/auth/auth.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthService } from './auth.service';
 
 @NgModule({
   imports: [
-    CommonModule, RouterModule.forChild([
+    CommonModule, ReactiveFormsModule, RouterModule.forChild([
       { path: 'login', component: LoginComponent },
       { path: 'register', component: RegisterComponent },
     ])
