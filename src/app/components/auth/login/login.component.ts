6d9636367e31100324ce7d9937ab60a67107d5c1
Ramprits <rampritsahani@outlook.in>
2019-06-28 19:01:19 +0530

    2 files modified

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -15,8 +15,8 @@ export class LoginComponent implements OnInit {
   }
   buildLoginForm() {
     this.loginForm = this.fb.group({
-      username: ["",],
-      password: ["",],
+      username: ["", Validators.required],
+      password: ["", Validators.required],
     })
   }
   onSubmit() { }
